6db8d20ef606605b4ef51f4b9c84ef9c52b2876d
roboticsDev1584 <matthewcalda@gmail.com>
2023-02-03 19:53:23 -0600

    Demo auton without path planner

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -18,6 +19,11 @@ public final class Constants {
   public static final int DriveRightLeader = 1;
   public static final int DriveRightFollower = 2;
   public static final double degreesAllowed = 2.5;
+  public static final double maxVoltage = 10;
+  public static final double maxSpeed = 3; // m/s
+    public static final double maxAcceleration =  3;//m/s^2
+  public static final double trackWidth = .599426775;
+  public static final DifferentialDriveKinematics kinematics = new DifferentialDriveKinematics(Constants.trackWidth);
   public static class DriverConstants {
     public static final int kDriverControllerPort = 0;
 
@@ -28,7 +34,8 @@ public final class Constants {
   public static class AutonConstants{
     public static final double kS = 0.21842;
     public static final double kV = 0.010489;
-    public static final double kA = 0.0043684;  
+    public static final double kA = 0.0043684;
+    public static final double kP = 0.0115;
   }
   public static class ManipulatorConstants {
 
--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -3,18 +3,71 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
+import frc.robot.Constants;
+
+import java.util.List;
 
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Drive;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
 
-public final class Autos {
-  /** Example static factory for an autonomous command. */
-  public static CommandBase exampleAuto(ExampleSubsystem subsystem) {
-    return Commands.sequence(new ExampleCommand(subsystem));
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.RamseteController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.trajectory.constraint.DifferentialDriveVoltageConstraint;
+
+public class Autos extends CommandBase {
+  RamseteCommand ramseteCommand;
+  Drive drive;
+
+  public Autos(Drive drive) {
+    this.drive = drive;
+    SimpleMotorFeedforward motorFF = new SimpleMotorFeedforward(Constants.AutonConstants.kS, Constants.AutonConstants.kV, Constants.AutonConstants.kA);
+    var autoVoltageConstraint = 
+      new DifferentialDriveVoltageConstraint(
+        motorFF, 
+        Constants.kinematics, Constants.maxVoltage);
+
+    TrajectoryConfig config = 
+      new TrajectoryConfig(Constants.maxSpeed, Constants.maxAcceleration)
+          .setKinematics(Constants.kinematics).addConstraint(autoVoltageConstraint);
+
+    Trajectory tragic = TrajectoryGenerator.generateTrajectory(
+      new Pose2d(0,0,new Rotation2d(0)), 
+      List.of(
+        new Translation2d(2,-1),
+        new Translation2d(3,0),
+        new Translation2d(4,1)
+      ), 
+      new Pose2d(6,0, new Rotation2d(0)), 
+      config);
+
+    drive.resetOdometry(tragic.getInitialPose());
+
+    RamseteController ramseteControl = new RamseteController();
+
+    ramseteCommand = new RamseteCommand(
+      tragic, 
+      drive::getPose,
+      ramseteControl, 
+      motorFF, 
+      Constants.kinematics, 
+      drive::getWheelSpeeds, 
+      new PIDController(Constants.AutonConstants.kP, 0, 0), 
+      new PIDController(Constants.AutonConstants.kP, 0, 0), 
+      drive::SetSpeed, 
+      drive);
   }
 
-  private Autos() {
-    throw new UnsupportedOperationException("This is a utility class!");
+  public CommandBase getRamseteCommand (){
+    return ramseteCommand.andThen(() -> drive.SetSpeed(0, 0));
   }
 }
