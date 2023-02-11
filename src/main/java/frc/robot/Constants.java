e02d6d910b6327370a12f94d2dbd1e03a8949dc4
Nimo <nimoohayonr@gmail.com>
2023-02-11 16:47:20 -0600

    chanbged to double solenoid, tuned constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -43,17 +43,19 @@ public final class Constants {
     public static final double kManipulatorControllerDeadZone = 0.1;
 
     public static final double armMaxSpeed = 0.3;
-    public static final double manipulatorSpeed = 0.5;
+    public static final double manipulatorSpeed = 0.75;
 
     public static final int armLeaderPort = 21;
     public static final int armFollower1Port = 22;
     public static final int armFollower2Port = 23;
     public static final int manipulatorPort = 24;
     public static final int intakeLimitPort = 1;
+
     public static final int kClaw1Solenoid = 0;
     public static final int kClaw2Solenoid = 1;
     public static final int kClaw3Solenoid = 2;
-
+    public static final int kClaw4Solenoid = 3;
+    
     public static final int armEncoderPort = 0;
 
     public static final double maxVoltage = 4.0;
--- src/main/java/frc/robot/subsystems/Manipulator.java
+++ src/main/java/frc/robot/subsystems/Manipulator.java
@@ -9,6 +9,7 @@ import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ManipulatorConstants;
@@ -16,47 +17,39 @@ import frc.robot.Constants.ManipulatorConstants;
 public class Manipulator extends SubsystemBase {
   private final static CANSparkMax manipulatorMotor = new CANSparkMax(ManipulatorConstants.manipulatorPort, MotorType.kBrushless);
   private final static DigitalInput manipulatorSwitch = new DigitalInput(ManipulatorConstants.intakeLimitPort);
-  private final static Solenoid claw1 = new Solenoid(PneumaticsModuleType.REVPH, ManipulatorConstants.kClaw1Solenoid);
-  private final static Solenoid claw2 = new Solenoid(PneumaticsModuleType.REVPH, ManipulatorConstants.kClaw2Solenoid);
-  private final static Solenoid claw3 = new Solenoid(PneumaticsModuleType.REVPH, ManipulatorConstants.kClaw3Solenoid);
+  private final static DoubleSolenoid claw1 = new DoubleSolenoid(PneumaticsModuleType.REVPH, ManipulatorConstants.kClaw1Solenoid, ManipulatorConstants.kClaw2Solenoid);
+  private final static DoubleSolenoid claw2 = new DoubleSolenoid(PneumaticsModuleType.REVPH, ManipulatorConstants.kClaw3Solenoid, ManipulatorConstants.kClaw4Solenoid);
 
+  
   /** Creates a new Manipulator. */
   public Manipulator() {
     manipulatorMotor.setIdleMode(IdleMode.kBrake);
 
-    claw1.set(false);
-    claw2.set(false);
-    claw3.set(false);
+    claw1.set(DoubleSolenoid.Value.kReverse);
+    claw2.set(DoubleSolenoid.Value.kReverse);
   }
 
   public void setIntake(double speed) {
     manipulatorMotor.set(speed); 
   }
 
-  public void setClaw(){
-    claw1.set(!claw1.get());
-    claw2.set(!claw2.get());
-  }
-
   public void setClaw(int configType) {
     if (configType == 1) {
-      claw1.set(true);
-      claw2.set(false);
-      claw3.set(true);
+      claw1.set(DoubleSolenoid.Value.kForward);
+      claw2.set(DoubleSolenoid.Value.kReverse);
     } else if (configType == 2) {
-      claw1.set(true);
-      claw2.set(true);
-      claw3.set(false);
+      claw1.set(DoubleSolenoid.Value.kForward);
+      claw2.set(DoubleSolenoid.Value.kForward);
     } if (configType == 3) {
-      claw1.set(false);
-      claw2.set(false);
-      claw3.set(true);
+      claw1.set(DoubleSolenoid.Value.kReverse);
+      claw2.set(DoubleSolenoid.Value.kForward);
     }
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    System.out.println(claw1.get());
   }
   
   public boolean cancelIfLimitTriggered() {
